874b066b42a6642e05499b7ad27646d4f2743973
Tim Liu <timliu@hk1.ibm.com>
2015-03-19 11:26:34 +0800

    1) refactor to a jQuery plugin. 2) performance fix

--- js/sb_collection.js
+++ js/sb_collection.js
@@ -1,4 +1,4 @@
-var loadCollection = function() {
+$.fn.loadCollection = function() {
 	var index = window.location.href.indexOf("#");
 	if (index >= 0) {
 		var collectionStr = window.location.href.substring(index+1);
@@ -7,16 +7,16 @@ var loadCollection = function() {
 			var row = collection >> 5;
 			var col = collection & 31;
 			for (var j=0; j<5; j++) {
-				$("div:nth-of-type(" + (row+1) + ") > button:nth-of-type(" + (j+1) + ")").toggleClass("selected", !!(col&(1<<j)));
+				$("div:nth-of-type(" + (row+1) + ") > button:nth-of-type(" + (j+1) + ")", this).toggleClass("selected", !!(col&(1<<j)));
 			}
 		});
 	}
 };
 
-var saveCollection = function() {
+$.fn.saveCollection = function() {
 	var collection = "";
 	var selected = 0;
-	$("button").each(function(i, button) {
+	$("button", this).each(function(i, button) {
 		var cur = $(button).hasClass("selected") ? 1 : 0;
 		selected |= cur << (i%5);
 		if (i % 5 == 4) {
@@ -30,27 +30,31 @@ var saveCollection = function() {
 	window.location.href = "#" + collection.substring(0, collection.length-1);
 };
 
-$(document).ready(function() {
-	var img = $("<img>");
-	img.attr("src", "./SB_collection_selected.png");
-	var w = img[0].naturalWidth / 5;
-	var r = img[0].naturalHeight / w; 
+$.fn.SBCollection = function() {
 
-	var container = $(".container");
-	for (var i=0; i<r; i++) {
-		var row = $("<div></div>");
-		for (var j=0; j<5; j++) {
-			var button = $("<button></button>");
-			button.css("width", w);
-			button.css("height", w);
-			button.css("background-position", (j*-102) + "px " + (i*-102) + "px");
-			row.append(button);
+	var container = this;
+
+	$("<img>", { src: "./SB_collection_selected.png" }).load(function(e) {
+		var w = e.target.naturalWidth / 5;
+		var r = e.target.naturalHeight / w; 
+
+		for (var i=0; i<r; i++) {
+			var row = $("<div></div>");
+			for (var j=0; j<5; j++) {
+				$("<button></button>", {
+					css: {
+						width: w,
+						height: w,
+						"background-position":  (j*-w) + "px " + (i*-w) + "px"
+					},
+					click: function() {
+						$(this).toggleClass("selected");
+						container.saveCollection();
+					}
+				}).appendTo(row);
+			}
+			container.append(row);
 		}
-		container.append(row);
-	}
-	$("button").click(function() {
-		$(this).toggleClass("selected");
-		saveCollection();
+		container.loadCollection();
 	});
-	loadCollection();
-});
+};
